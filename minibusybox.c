bf99c672b22dd7bd088cb056189f2e62d684f919
Jorge Aragon <jorge.aravill@gmail.com>
2023-04-30 15:57:38 -0600

    Adding cgantges to main

--- minibusybox.c
+++ minibusybox.c
@@ -7,6 +7,7 @@ int main(int argc, char** argv){
 	// strchr() returns the last appearance of a character
 	// If the name contains a / it means the program's name starts after the last slash
 	// If not, it means the program's name starts on argv[0][0] (i.e. executed from $PATH).
+	
 	char* command = strchr(argv[0], '/');
 	if(command == NULL)
 		command = argv[0];
@@ -14,9 +15,11 @@ int main(int argc, char** argv){
 		command += 1;
 	if(strcmp(command, "minibusybox") == 0)
 		printf("Available commands:\n - sleep_\n - cat_\n - uname_\n - lsmod_\n");
-	else if(argc == 2){
+	else if(argc == 1){
 		if(strcmp(command, "uname_") == 0)
-			return sleepSeconds(argv[1]);
+		{
+			unameValidation(argc,argv);
+		}
 		else
 			printf("Unknown command\n");
 	}else
